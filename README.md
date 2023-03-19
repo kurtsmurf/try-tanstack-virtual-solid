2ec5e14995cfb7e1f20293d1f72e22c39d5fb08f
Kurt <chrisronsmith@gmail.com>
2023-03-19 19:14:52 -0400

    it work

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+[https://github.com/solidjs/solid/discussions/512](https://github.com/solidjs/solid/discussions/512)
+
+[https://codesandbox.io/embed/virtualized-list-9dw9q](https://codesandbox.io/embed/virtualized-list-9dw9q)
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { createVirtualizer } from "@tanstack/solid-virtual"
+
+
+const App = () => {
+  let parentRef;
+
+  const rowVirtualizer = createVirtualizer({
+    count: 1000,
+    getScrollElement: () => parentRef,
+    estimateSize: () => 35,
+  });
+
+  return (
+    <>
+      {/* The scrollable element for your list */}
+      <div
+        ref={parentRef}
+        style={{
+          height: `400px`,
+          overflow: 'auto', // Make it scroll!
+        }}
+      >
+        {/* The large inner element to hold all of the items */}
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          {/* Only the visible items in the virtualizer, manually positioned to be in view */}
+          {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+            <div
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${virtualItem.start}px)`,
+              }}
+            >
+              Row {virtualItem.index}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default App;
